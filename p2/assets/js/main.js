c6be78626d19af6041e9cea5da85c8d19cc724ce
mg <amg.vergara@gmail.com>
2022-09-27 23:01:38 -0300

    Se agrega la parte 2 del desafio

--- p2/assets/js/main.js
+++ p2/assets/js/main.js
@@ -1,9 +1,34 @@
-const div = document.querySelector("#photo");
+const cantidadSticker1 = document.querySelector("#sticker1")
+const cantidadSticker2 = document.querySelector("#sticker2")
+const cantidadSticker3 = document.querySelector("#sticker3")
+const respuesta = document.querySelector('h2')
 
-div.addEventListener("click", function () {
-  if (div.classList.contains("borde") == true) {
-    div.classList.remove("borde");
-  } else {
-    div.classList.add("borde");
+
+let boton = document.querySelector("#boton")
+
+boton.addEventListener("click", function () {
+
+  console.log("Evento recibido");
+  console.log(cantidadSticker1.value);
+  console.log(cantidadSticker2.value);
+  console.log(cantidadSticker3.value);
+
+  let totalStickers = suma(cantidadSticker1.value,cantidadSticker2.value,cantidadSticker3.value)
+
+  console.log(totalStickers)
+  console.log(respuesta.value)
+
+  if(totalStickers <= 10)
+  {
+    console.log(`Llevas ${totalStickers} stickers`)
+    respuesta.innerHTML = `Llevas ${totalStickers} stickers`
+  }else{
+    console.log("Llevas demasiados stickers")
+    respuesta.innerHTML = 'Llevas demasiados stickers'
   }
 });
+
+
+function suma (a,b,c){
+  return ( Number(a) + Number(b) + Number(c))
+}
